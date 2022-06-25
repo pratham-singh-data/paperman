54a6c22037c4f223b3723c4267fe7ea038d2b79e
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-06-25 17:01:22 +0530

    News Pulled

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getResults } from "../misc";
 
 const Business = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        getResults(null, "us", "business").then(item => setArticles(item.articles));
+    }, [])
+
+    let iter = 0;
     return(
-        <div>This is business page</div>
+        <div>
+            {articles.map(({source, author, title, description, url, urlToImage, publishedAt}) => {
+                return(
+                    <div key={iter++}>
+                        <h2>{title}</h2>
+                        <div>
+                            {source.name}
+                            {publishedAt}
+                        </div>
+                        <img src={urlToImage} alt={title}/>
+                        <div>
+                            <p>{author}</p>
+                            <p>{description}</p>
+                        </div>
+                        <hr/>
+                    </div>
+                )
+            })}
+        </div>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getResults } from "../misc";
 
 const Home = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        getResults(null, "us", "general").then(item => setArticles(item.articles));
+    }, [])
+
+    let iter = 0;
     return(
-        <div>This is home page</div>
+        <div>
+            {articles.map(({source, author, title, description, url, urlToImage, publishedAt}) => {
+                return(
+                    <div key={iter++}>
+                        <h2>{title}</h2>
+                        <div>
+                            {source.name}
+                            {publishedAt}
+                        </div>
+                        <img src={urlToImage} alt={title}/>
+                        <div>
+                            <p>{author}</p>
+                            <p>{description}</p>
+                        </div>
+                        <hr/>
+                    </div>
+                )
+            })}
+        </div>
     );
 }
 
